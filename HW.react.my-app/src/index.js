5253811c086a18b7f544fe2a07d133623e533d56
Admin777 <pbelyi357@gmail.com>
2022-10-06 17:10:01 +0300

    redux

--- HW.react.my-app/src/index.js
+++ HW.react.my-app/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import './components/HW1/HW1.css';
 import App from './App';
 import HW1 from './components/HW1/HW1';
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
+ <Provider store={}>
         <App data= {<HW1/>}/>
-    </React.StrictMode>
+ </Provider>
+
+    // </React.StrictMode>
 );
 
