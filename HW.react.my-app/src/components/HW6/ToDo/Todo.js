1c5ce7c38d0896158390205b8b46431f4d045ce1
Admin <pbelyi357@gmail.com>
2022-10-18 20:11:01 +0300

    HW6

--- HW.react.my-app/src/components/HW6/ToDo/Todo.js
+++ HW.react.my-app/src/components/HW6/ToDo/Todo.js
@@ -1,22 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {TodoAction} from "./todoAction";
+import axios from "axios";
 
 const Todo = (props) => {
     const dispatch = useDispatch();
 
-    const [todoTitle, setTodoTitle] = useState("");
+    let [todoTitle, setTodoTitle] = useState();
     useEffect(()=>{
         dispatch(TodoAction())
+        axios('https://jsonplaceholder.typicode.com/todos')
+            .then(res =>
+                setTodoTitle(res.data)
+            )
+
         },[])
 
+    const todosMap = todoTitle?.map((todo)=> <p className={"p"} key={todo.id}>{todo.title} </p>)
+
     const todos = useSelector((store)=>(store.todos));
-    const todosMap = todos.todos.map((todo, i) => {
+    const todosMapRedux = todos.todos.map((todo) => {
         return <p className={"p"} key={todo.id}>{todo.title} </p>
     })
 
     return (
         <div>
+            <h1>TODOS</h1>
+            {todosMapRedux}
             <h1>TODOS</h1>
             {todosMap}
         </div>
